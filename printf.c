266dab4f950306e864f40822e7dad4fcd0c3c89f
monwabisindlovu <monwabisindlovu78@gmail.com>
2023-06-26 11:44:00 -0700

    printf.c

--- printf.c
+++ printf.c
@@ -1,6 +1,48 @@
 #include "main.h"
 
 /**
+ * print_format - Prints a character or string depending on the
+ * format specifier.
+ *
+ * @format: A character string containing directives.
+ * @args: A va_list containing the arguments.
+ *
+ * Return: The number of characters printed.
+ */
+int print_format(const char *format, va_list args)
+{
+int printed_chars = 0;
+
+switch (*format)
+{
+case 'c':
+printed_chars += _putchar(va_arg(args, int));
+break;
+case 's':
+{
+char *str = va_arg(args, char *);
+if (str == NULL)
+str = "(null)";
+while (*str)
+{
+printed_chars += _putchar(*str);
+str++;
+}
+}
+break;
+case '%':
+printed_chars += _putchar('%');
+break;
+default:
+printed_chars += _putchar('%');
+printed_chars += _putchar(*format);
+break;
+}
+
+return (printed_chars);
+}
+
+/**
  * _printf - Produces output according to a format.
  * @format: A character string containing directives.
  *
@@ -20,44 +62,12 @@ while (*format)
 {
 if (*format != '%')
 {
-_putchar(*format);
-printed_chars++;
+printed_chars += _putchar(*format);
 }
 else
 {
 format++;
-
-switch (*format)
-{
-case 'c':{
-char c = va_arg(args, int);
-_putchar(c);
-printed_chars++;
-break;
-}
-case 's':
-{
-char *str = va_arg(args, char *);
-if (str == NULL)
-str = "(null)";
-while (*str)
-{
-_putchar(*str);
-printed_chars++;
-str++;
-}
-break;
-}
-case '%':
-_putchar('%');
-printed_chars++;
-break;
-default:
-_putchar('%');
-_putchar(*format);
-printed_chars += 2;
-break;
-}
+printed_chars += print_format(format, args);
 }
 format++;
 }
